14230c843c36b0ca10c4807fc0b2cc6cfde44863
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-07-30 11:41:41 +0530

    code was refactored.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,31 +1,8 @@
-import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
-import { getStudents } from '../api';
-import * as constants from '../actions/actionTypes';
-
-// worker Saga: will be fired on STUDENTS_FETCH_REQUESTED actions
-function* fetchStudents(action) {
-   try {
-      const students = yield call(getStudents);
-      yield put({type: constants.STUDENTS_FETCH_SUCCEEDED, students: students});
-   } catch (e) {
-      yield put({type: constants.STUDENTS_FETCH_FAILED, message: e.message});
-   }
-}
-
-/*
-  Starts fetchStudents on each dispatched `STUDENTS_FETCH_REQUESTED` action.
-  Allows concurrent fetches of user.
-*/
-function* studentsSaga() {
-  yield takeEvery(constants.STUDENTS_FETCH_REQUESTED, fetchStudents);
-}
-
+import { studentsSaga } from './studentSaga';
 // single entry point to start all Sagas at once
 export function* rootSaga() {
   yield [
     studentsSaga()
   ]
 }
-
-
 export default rootSaga;
---
+++ src/sagas/studentSaga.js
@@ -0,0 +1,21 @@
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import { getStudents } from '../api';
+import * as constants from '../actions/actionTypes';
+
+// worker Saga: will be fired on STUDENTS_FETCH_REQUESTED actions
+function* fetchStudents(action) {
+   try {
+      const students = yield call(getStudents);
+      yield put({type: constants.STUDENTS_FETCH_SUCCEEDED, students: students});
+   } catch (e) {
+      yield put({type: constants.STUDENTS_FETCH_FAILED, message: e.message});
+   }
+}
+
+/*
+  Starts fetchStudents on each dispatched `STUDENTS_FETCH_REQUESTED` action.
+  Allows concurrent fetches of user.
+*/
+export function* studentsSaga() {
+  yield takeEvery(constants.STUDENTS_FETCH_REQUESTED, fetchStudents);
+}

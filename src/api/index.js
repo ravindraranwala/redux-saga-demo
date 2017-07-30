6a7b3a44aebc5d370b0406c057502cbfdfcc742b
Ranwala, Ravindra <Ravindra.Ranwala@pearson.com>
2017-07-30 23:19:43 +0530

    endpoint was updated.

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export function getStudents() {
-    return fetch('http://www.mocky.io/v2/597ade4b1200008404696c6b')
+    return fetch('http://www.mocky.io/v2/597e1a720f0000d50906ecb6')
     .then(students => students.json().then(data => {
         return data})).catch(e => {throw e})
 }
